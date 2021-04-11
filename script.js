2735705d4dbdf153de115f1ca72e0d02c56323ce
Rhez Danielle <rdpmanliguez@mymail.mapua.edu.ph>
2021-04-12 06:52:08 +0800

    Updated mobile design

--- script.js
+++ script.js
@@ -11,25 +11,27 @@ function forgot_password(){
     sky.style.filter = "none";
     bg.style.animation = "night-sky 2s forwards ease";
     sun.style.animation = "moon 2s forwards ease";
-    let retrievePass = false;
    
         //  transition animation from login to retrieve password //
-    if(retrievePass == false){
+ 
     signIn.style.animations = "swap-form 2s forwards ease";
     signIn.style.display = 'none';
     forgotPass.style.display = 'block'
 
-    retrievePass = true;
-    }
+    return false;
+};
 
-    if(retrievePass != true){
-        signIn.style.animations = "swap-form 2s forwards ease";
+
+function sign_in(){
+    var sun = document.getElementById('sun');
+    var sky = document.getElementById('clouds');
+    var forgotPass = document.getElementById('forgot-password-form');
+    var signIn = document.getElementById("login-form");
+    var bg = document.documentElement;
+
+    signIn.style.animations = "swap-form 2s forwards ease";
         signIn.style.display = 'none';
         forgotPass.style.display = 'block'
 
         retrievePass = false;
-        }
-
-
-    return false;
-};
+}
